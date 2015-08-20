902b8d241a1664ce641defb37c39b4b5cd3d755c
PokeTheCity <pokethecity@protonmail.ch>
2015-08-20 15:40:07 +0200

    add monitoring function and calculate cycle duration

--- plants/bottle-filling/get_info.py
+++ plants/bottle-filling/get_info.py
@@ -5,43 +5,45 @@ from pymodbus.exceptions import ConnectionException
 import time
 import logging
 import json
+import argparse
+import sys
 
 #####################################
 # Code
 #####################################   
-INTERVAL = 1
-DURATION = 60
-CYCLE_LENGTH = 15
+#INTERVAL = 1
+#DURATION = 60
+#CYCLE_LENGTH = 15
 data = {}
 
-def get_data():
-    client = ModbusClient('localhost', port=5020)
-    try:
-        client.connect()
-        for i in range(DURATION):
-            time.sleep(INTERVAL)
-            rr = client.read_holding_registers(0x1, 4)
-            data[i] = {
-                    "levelSensor" : rr.registers[0], 
-                    "limitSwitch" : rr.registers[1],
-                    "motor" : rr.registers[2],
-                    "nozzle" : rr.registers[3]
-                    }
-        with open('states_data.txt', 'w') as fd:
-            json.dump(data, fd)
-    except KeyboardInterrupt:
-        client.close()
-    except ConnectionException:
-        print "Unable to connect / Connection lost"
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--initiate", type=int, help="Calibrate the operating cycle (3 arguments expected: interval cycle_length and duration)", nargs='+')
+    parser.add_argument("-m", "--monitor", help="If initiated, start the monitoring", action="store_true")
 
-def parse_data():
+    return parser.parse_args()
+
+def get_data(client, interval, duration):
+    for i in range(duration):
+        time.sleep(interval)
+        rr = client.read_holding_registers(0x1, 4)
+        data[i] = {
+                  "levelSensor" : rr.registers[0], 
+                   "limitSwitch" : rr.registers[1],
+                   "motor" : rr.registers[2],
+                   "nozzle" : rr.registers[3]
+                   }
+    with open('states_data.txt', 'w') as fd:
+        json.dump(data, fd)
+
+def parse_data(cycle_length):
     data = {}
     result = {}
     with open('states_data.txt', 'r') as fd:
         data = json.load(fd)
 
     for i in data.keys():
-        j = int(i) % CYCLE_LENGTH
+        j = int(i) % cycle_length
         if j not in result:
             result[j] =  {
                    "levelSensor" : [data[i]['levelSensor']],
@@ -64,5 +66,80 @@ def parse_data():
 
     for state in result:
         print result[state]
+    
+    with open('states_result.txt', 'w') as fr:
+            json.dump(result, fr)
 
-parse_data()
+def monitor(client, interval, cycle_length):
+    with open('states_result.txt', 'r') as fr:
+        states = json.load(fr)
+    j = 0
+    while True :
+        time.sleep(interval)
+        distance = 0
+        rr = client.read_holding_registers(0x1, 4)
+        print j
+        print states[str(j)]
+        if rr.registers[0] not in states[str(j)]['levelSensor']:
+            print "Alert: unexpected state: levelSensor"
+            distance = distance+1
+        if rr.registers[1] not in states[str(j)]['limitSwitch']:
+            print "Alert: unexpected state: limitSwitch"
+            distance = distance+1
+        if rr.registers[2] not in states[str(j)]['motor']:
+            print "Alert: unexpected state: motor"
+            distance = distance+1
+        if rr.registers[3] not in states[str(j)]['nozzle']:
+            print "Alert: unexpected state: nozzle"
+            distance = distance+1
+        if distance > 0:
+            print "Distance from normal state: "+str(distance)
+
+        j = (j+1)%cycle_length
+
+def wait_for_beginning(client):
+    was_zero = True
+    while True:
+        try:
+            rr = client.read_holding_registers(0x2, 1)
+            if rr.registers[0] == 1:
+                if was_zero:
+                    return
+            was_zero = rr.registers[0] == 0
+        except KeyboardInterrupt:
+            return
+        except ConnectionException:
+            return
+
+def get_cycle_length(client):
+    wait_for_beginning
+    start_time = time.time()
+    wait_for_beginning(client)
+    return time.time() - start_time
+
+if __name__ == "__main__":
+    client = ModbusClient('localhost', port=5020)
+    try:
+        client.connect()
+        args = parse_args()
+        if args.initiate:
+            if len(args.initiate) != 2:
+                print "Error, -i (--initiate) takes two arguments: interval and iteration"
+                sys.exit(0)
+            print "Start initialization"
+            print "Measuring cycle duration"
+            cycle_length = int(get_cycle_length(client))
+            print "Cycle duration: "+str(cycle_length)
+            if cycle_length > args.initiate[1]:
+                print "Error, cycle length must be less than duration"
+                sys.exit(0)
+            get_data(client, args.initiate[0], args.initiate[1])
+            print "Measures done"
+            parse_data(cycle_length)
+        elif args.monitor:
+            monitor(client, 1, 15)
+    except KeyboardInterrupt:
+        client.close()
+    except ConnectionException:
+        print "Unable to connect / Connection lost"
+    sys.exit(0)
