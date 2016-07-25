957c0275efbfe5094dc5dcf341cc6f0c5feb85d7
Ike <ikeclinton@gmail.com>
2016-07-25 17:55:03 +0000

    Added new setCommand func

--- plants/oil-refinery/oil_hmi.py
+++ plants/oil-refinery/oil_hmi.py
@@ -51,8 +51,8 @@ class HMIWindow(Gtk.Window):
         feed_pump_start_button = Gtk.Button("START")
         feed_pump_stop_button = Gtk.Button("STOP")
         
-        feed_pump_start_button.connect("clicked", setCommand(0x10), 1)
-        feed_pump_stop_button.connect("clicked", setCommand(0x10), 0)
+        feed_pump_start_button.connect("clicked", self.setCommand(0x10), 1)
+        feed_pump_stop_button.connect("clicked", self.setCommand(0x10), 0)
         
         feed_pump_start_button.connect("clicked", self.setProcess, 1)
         feed_pump_stop_button.connect("clicked", self.setProcess, 0)
@@ -165,7 +165,7 @@ class HMIWindow(Gtk.Window):
         except:
             pass
         
-    def setCommand(register, data=None):
+    def setCommand(self, widget, register, data=None):
         try:
             self.modbusClient.write_register(register, data)
         except:
