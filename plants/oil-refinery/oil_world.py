93bf3a65e28ed5d4eb30900df46841e69696fcd9
Ike <ikeclinton@gmail.com>
2016-08-02 19:40:23 +0000

    Adding sensor for spilled oil

--- plants/oil-refinery/oil_world.py
+++ plants/oil-refinery/oil_world.py
@@ -143,8 +143,8 @@ def oil_spill_sensor(space):
     body = pymunk.Body()
     body.position = (0, 550)
     radius = 7
-    a = (0, 400)
-    b = (640, 400)
+    a = (0, 100)
+    b = (640, 100)
     shape = pymunk.Segment(body, a, b, radius)
     shape.collision_type = oil_spill_collision # tank_level
     space.add(shape)
