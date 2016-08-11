55902fba1acf32809dfbbd0128bea70daa34fc90
Ike <ikeclinton@gmail.com>
2016-08-11 00:26:22 +0000

    separator vessel

--- plants/oil-refinery/oil_world.py
+++ plants/oil-refinery/oil_world.py
@@ -247,7 +247,7 @@ def add_oil_unit(space):
                 l16, l17, l18, l19, l20, l21, l22, l23, l24, l25, 
                 l26, l27, l28, l29, l30, l31, l32, l33, l34) # 3
 
-    return (l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17
+    return (l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11,l12,l13,l14,l15,l16,l17,
         l18,l19,l20,l21,l22,l23,l24,l25,l26,l27,l28,l29,l30,
         l31,l32,l33,l34)
 
