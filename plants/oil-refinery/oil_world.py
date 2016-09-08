c6646e926071834b0f6a5c796f2a1f831809994f
Ike <ikeclinton@gmail.com>
2016-09-08 15:46:18 +0000

    changing sensor colors

--- plants/oil-refinery/oil_world.py
+++ plants/oil-refinery/oil_world.py
@@ -457,7 +457,8 @@ def run_world():
         draw_line(bg, sep_valve_obj)
         draw_line(bg, outlet)
         draw_line(bg, waste_valve_obj)
-        draw_line(bg, oil_spill, THECOLORS['white'])
+        draw_line(bg, oil_spill, THECOLORS['red'])
+        draw_line(bg, oil_process, THECOLORS['black'])
 
         #draw_ball(screen, separator_feed, THECOLORS['red'])
         title = fontMedium.render(str("Crude Oil Pretreatment Unit"), 1, THECOLORS['blue'])
